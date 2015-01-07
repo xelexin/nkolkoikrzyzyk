3e351e14fab5c643af603b72fd0dc8702e39148a
Witek902 <witek902@gmail.com>
2015-01-07 22:09:45 +0100

    NeuralNetworkPlayer switched to use NeuralNetwork

--- src/nkolkoikrzyzyk/model/NeuralNetworkPlayer.java
+++ src/nkolkoikrzyzyk/model/NeuralNetworkPlayer.java
@@ -1,38 +1,62 @@
 package nkolkoikrzyzyk.model;
 
+import java.util.Arrays;
+import java.util.Random;
+
 public class NeuralNetworkPlayer extends Player 
 {
-	public static neurons siecX = new neurons(0.5, Mark.CROSS.value());
-	public static neurons siecO = new neurons(0.5, Mark.NOUGHT.value());
-	
-	private neurons siec;
-	
 	private NeuralNetwork network;
+	private Mark markType;
+	private Random random;
 		
 	public NeuralNetworkPlayer(String name, Mark markType, NeuralNetwork network) {
 		super(name, markType);
 		this.network = network;
-		siec = markType==Mark.CROSS?siecX:siecO;
+		this.markType = markType;
+		random = new Random();
 	}
 
 	@Override
 	public int[] makeMove( int[] board, int position ) {
-		board = siec.outputSignal(board);
+		int bestMove = -1;
+		
+		if (network != null) {
+			float[] input = new float[9];
+			for (int i = 0; i < 9; i++)
+				input[i] = (float)board[i];
+			// TODO: conversion from Xs to Os will be probably needed
+			
+			float[] output = network.run(input);
+			System.out.println("Neural network output = " + Arrays.toString(output));
+			
+			float bestProbability = Float.NEGATIVE_INFINITY;
+			for (int i = 0; i < 9; i++)
+				if (output[i] > bestProbability && board[i] == 0) {
+					bestMove = i;
+					bestProbability = output[i];
+				}		
+		} else
+			System.out.println("network == null, doing random move...");
+		
+		if (bestMove == -1)
+			do {
+				bestMove = random.nextInt(9);
+			} while (board[bestMove] == 0);
+
+		System.out.println("Neural network player selected " + bestMove + " as a best move");
+		board[bestMove] = markType.value();
 		return board;
 	}
 
 	@Override
 	public void youWin(int[] board) {
-		siec.youWin(board);
 	}
 
 	@Override
 	public void youLost() {
-		siec.youLost();
 	}
 	
 	@Override
 	public void youDraw(int[] board) {
-		siec.youWin(board);
 	}
 }
--- src/nkolkoikrzyzyk/model/Player.java
+++ src/nkolkoikrzyzyk/model/Player.java
@@ -26,6 +26,12 @@ public abstract class Player
 		return markType;
 	}
 	
+	/**
+	 * Make move
+	 * @param board		Current board state.
+	 * @param position	Where mark is being put? (used only by Human player)
+	 * @return			Board state after executing a move.
+	 */
 	public abstract int[] makeMove(int[] board, int position);
 	public abstract void youWin(int[] board);
 	public abstract void youDraw(int[] board);
